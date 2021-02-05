8a66c9a3ec8421c6757cf4f3e9544800c81ec5a8
8899871637adarsh <8899871637adarsh@gmail.com>
2021-02-06 00:27:47 +0530

    Template

---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "../Hero";
+import "../../App.css";
+import Cards from "../Cards";
+import Footer from "../Footer";
+
+
+const Home = () =>{
+    return (
+        <>
+            <Hero />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
